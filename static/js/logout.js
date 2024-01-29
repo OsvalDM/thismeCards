49250025dff6a520e8e73bfc0ea2c49f3469601b
OsvalDM <osvaldvm140303@gmail.com>
2024-01-28 21:09:11 -0600

    Fix: logout error 400

--- static/js/logout.js
+++ static/js/logout.js
@@ -17,7 +17,7 @@ const logout = async ()=> {
         }
 
         const data = await response.json();
-        window.location.href = '';
+        window.location.href = '/';
     }catch(err){
         console.log('Error en la solicitud: ', err.message);
     }
